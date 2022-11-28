90c128879c1a1d888e12c8eeba181decac9f3778
Beppin00 <beppin.cascone23@gmail.com>
2022-11-28 18:45:51 +0100

    #

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Esercizio37'`, () => {
+  it(`should have as title 'ESERCIZIO28'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Esercizio37');
+    expect(app.title).toEqual('ESERCIZIO28');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Esercizio37 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('ESERCIZIO28 app is running!');
   });
 });
--- src/app/todos.service.ts
+++
@@ -1,5 +0,0 @@
-import { Todo } from "./models/todo"
-
-export async function getTodos():Promise<Todo[]>{
-  return await (await fetch('assets/db.json')).json()
-}
